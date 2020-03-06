4733180b1b5ff5becdab45fc6ae1af81f48608b8
x0rzkov <x0rzkov@protonmail.com>
2020-03-06 07:29:50 +0100

    draft package structure

---
+++ search.go
@@ -0,0 +1,10 @@
+package search
+
+type SearchInterface interface {
+	Get(key string) (string, error)
+	Unmarshal(key string, object interface{}) error
+	Set(key string, value interface{}) error
+	Fetch(key string, fc func() interface{}) (string, error)
+	Delete(key string) error
+}
+
